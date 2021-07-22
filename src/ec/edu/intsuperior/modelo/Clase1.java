1d2cd965e8ee7871ff476b73b0c7a6d9f63144aa
Usuario <Usuario@DESKTOP-ADA5GUN>
2021-07-21 22:28:09 -0500

    Esta es la version 1 del proyecto de POO se han manejado tipo de datos

---
+++ src/ec/edu/intsuperior/modelo/Clase1.java
@@ -0,0 +1,26 @@
+
+package ec.edu.intsuperior.modelo;
+
+
+public class Clase1 {
+
+public static void metodosalida1(){
+
+    System.out.println("Hola como estas");
+
+}
+public static String metodoRetorno2(){
+    String x="pillas no te duermas";
+    return x;
+    
+}
+public void salida(String p){
+    
+    System.out.println(p);
+}
+
+
+
+}
+
+
---
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.vista;
+
+import ec.edu.intsuperior.modelo.Clase1;
+
+public class Aplicacion {
+    
+    public static void main(String[] args) {
+        Clase1 obj=new Clase1();
+        obj.salida(p);
+        
+   
+}
+    
+    
+    
+    
+}
